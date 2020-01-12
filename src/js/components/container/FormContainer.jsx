025063912c480d36f19cd6e0096124409d58fce1
Jose L. Almada <jalmada.e@gmail.com>
2020-01-11 22:35:34 -0500

    mare box

--- src/js/components/container/FormContainer.jsx
+++ src/js/components/container/FormContainer.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Icon from "../presentational/Icon.jsx";
 import IconInput from "../presentational/IconInput.jsx";
 import IconToggle from "../presentational/IconToggle.jsx";
@@ -12,85 +12,75 @@ import Servo from '../../controllers/servo';
 import StreamingCamera from '../../controllers/streamingCamera';
 
 
-class FormContainer extends Component {
-  constructor() {
-    super();
 
-    //Icons
-    this.gainIcon = "chess-board";
-    this.dimIcon = "sun";
-    this.darkOnTrue = ['far', 'moon'];
-    this.darkOnFalse = ['fas', 'moon'];
-    this.lightOnTrue = ['far', 'lightbulb'];
-    this.lightOnFalse = ['fas', 'lightbulb'];
-    this.photo = "camera"
+function FormContainer () {
 
-    this.state = {
-      gain: 0,
-      dim: 0,
-      toggle: {
-        darkOn: false,
-        lightOn: false
-      }
-    }; 
+  const [gain, setGain] = useState('');
+  const [dim, setDim] = useState('');
+  const [darkOn, setDarkOn] = useState(false);
+  const [lightOn, setLightOn] = useState(false);
 
-    this.streamingCamera = new StreamingCamera();
-    let dim = this.streamingCamera.getDim();
-    let gain = this.streamingCamera.getGain();
-
-    this.onClick = {
-      photo: () => {
-        this.streamingCamera.takePicture();
-      }
-    }
-    
-    this.handleInputChange = (e) => this.setState({...this.state, [e.currentTarget.id]:e.target.value}); 
-    this.handleToggleClick = (e) => this.setState({...this.state, toggle: {...this.state.toggle, [e.currentTarget.id]: !this.state.toggle[e.currentTarget.id]}});
-    this.handleClick = ((e) => this.onClick[e.currentTarget.id]()).bind(this);
+  let states = {
+    gain: {state: gain, setter :setGain},
+    dim: {state: dim, setter :setDim},
+    darkOn: {state: darkOn, setter :setDarkOn},
+    lightOn: {state: lightOn, setter :setLightOn}
   }
+  //Icons
+  let gainIcon = "chess-board";
+  let dimIcon = "sun";
+  let darkOnTrue = ['far', 'moon'];
+  let darkOnFalse = ['fas', 'moon'];
+  let lightOnTrue = ['far', 'lightbulb'];
+  let lightOnFalse = ['fas', 'lightbulb'];
+  let photo = "camera"
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row justify-content-end">
-          <div className="col-2" style={{paddingRight:'0px'}}>
-            <div className="btn-group float-right">
-              <IconToggle id = 'darkOn' iconTrue = {this.darkOnTrue} iconFalse = {this.darkOnFalse} value = {this.state.toggle.darkOn} handleClick = {this.handleToggleClick}  />
-              <IconToggle id = 'lightOn' iconTrue = {this.lightOnTrue} iconFalse = {this.lightOnFalse} value = {this.state.toggle.lightOn} handleClick = {this.handleToggleClick}  />
-              <Icon id='photo' icon={this.photo} handleClick= {this.handleClick} />
-            </div>
+  let streamingCamera = new StreamingCamera();
+
+  let handleInputChange = (e) => states[e.currentTarget.id].setter(e.target.value); 
+  let handleToggleClick = (e) => states[e.currentTarget.id].setter(!states[e.currentTarget.id].state);
+
+  let handleClick = (e) => {};
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-end">
+        <div className="col-2" style={{paddingRight:'0px'}}>
+          <div className="btn-group float-right">
+            <IconToggle id = 'darkOn' iconTrue = {darkOnTrue} iconFalse = {darkOnFalse} value = {darkOn} handleClick = {handleToggleClick}  />
+            <IconToggle id = 'lightOn' iconTrue = {lightOnTrue} iconFalse = {lightOnFalse} value = {lightOn} handleClick = {handleToggleClick}  />
+            <Icon id='photo' icon={photo} handleClick= {handleClick} />
           </div>
         </div>
-        <div className="row" style={{height:"304px"}}>
-          <img src={TestImage} alt="Probando 1..2..3" style={{position:'absolute', top:'0px', left:'0px', zIndex:-10}} />
-        </div>
-        <div className="row align-items-end">
-          <div style={{display:'table', width:'100%', height:'100%'}}>
-            <div style={{display:'table-cell', verticalAlign:'bottom'}}>
-              <ServoControlsContainer />
-            </div>
-            <div style={{display:'table-cell', verticalAlign:'bottom'}}>
-              <div className="container-fluid">
-                <div className="row align-items-end">
-                  <div className="col-3">
-                    <IconInput id='dim' type = "number" placeholder='Dim' icon={this.dimIcon} value = {this.state.dim} handleChange = {this.handleInputChange} handleEnterClick={this.streamingCamera.setDim.bind(this.streamingCamera)} />
-                  </div>
-                  <div className="col-3">
-                    <IconInput id='gain' type = "number" placeholder='Gain' icon={this.gainIcon} value = {this.state.gain} handleChange = {this.handleInputChange} handleEnterClick={this.streamingCamera.setGain.bind(this.streamingCamera)} />
-                  </div>
-                  <div className="col" style={{height:'54px', paddingRight:'0px'}}>
-                    <audio controls className="float-right" style={{width:'150px'}}>
-                      <source src="audio" type="audio/x-wav;codec=pcm" />
-                      Your browser does not support the audio element.
-                    </audio>
-                  </div>
+      </div>
+      <div className="row" style={{height:"304px"}}>
+        <img src={TestImage} alt="Probando 1..2..3" style={{position:'absolute', top:'0px', left:'0px', zIndex:-10}} />
+      </div>
+      <div className="row align-items-end">
+        <div style={{display:'table', width:'100%', height:'100%'}}>
+          <div style={{display:'table-cell', verticalAlign:'bottom'}}>
+            <ServoControlsContainer />
+          </div>
+          <div style={{display:'table-cell', verticalAlign:'bottom'}}>
+            <div className="container-fluid">
+              <div className="row align-items-end">
+                <div className="col-3">
+                  <IconInput id='dim' type = "number" placeholder='Dim' icon={dimIcon} value = {dim} handleChange = {handleInputChange} handleEnterClick={streamingCamera.setDim.bind(streamingCamera)} />
+                </div>
+                <div className="col-3">
+                  <IconInput id='gain' type = "number" placeholder='Gain' icon={gainIcon} value = {gain} handleChange = {handleInputChange} handleEnterClick={streamingCamera.setGain.bind(streamingCamera)} />
+                </div>
+                <div className="col" style={{height:'54px', paddingRight:'0px'}}>
+                  <audio controls className="float-right" style={{width:'150px'}}>
+                    <source src="audio" type="audio/x-wav;codec=pcm" />
+                    Your browser does not support the audio element.
+                  </audio>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 export default FormContainer;
